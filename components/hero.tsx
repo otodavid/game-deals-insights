dd6a35f0eb0b8f80217bb6fcfd04cf74017e774b
otodavid <dieavid01@gmail.com>
2025-09-15 22:09:22 -0500

    feat:updated hero section to match api data

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,14 +1,33 @@
+"use client";
+
 import Image from "next/image";
 import React from "react";
 import { Badge } from "./ui/badge";
 import { Flame } from "lucide-react";
 import { Button } from "./ui/button";
+import { useHeroGameDeal } from "@/lib/queries/useHeroGameDeal";
+import HeroSkeleton from "./hero-skeleton";
+import { ITADGame } from "@/types/api-responses";
 
 export default function HeroSection() {
+  const { data, isLoading, isError } = useHeroGameDeal();
+
+  if (isLoading) return <HeroSkeleton />;
+
+  if (isError || !data || data.list.length === 0) {
+    return (
+      <div className="w-full aspect-video rounded-md bg-muted">
+        <p>Something went wrong</p>
+      </div>
+    );
+  }
+
+  const gameDeal: ITADGame = data.list[0];
+
   return (
     <div className="w-full aspect-video relative rounded-md overflow-hidden isolate">
       <Image
-        src={"/images/hero.webp"}
+        src={gameDeal.assets.boxart}
         alt="Deal of the day image"
         fill={true}
         className="object-cover aspect-video -z-10"
@@ -22,11 +41,15 @@ export default function HeroSection() {
         </div>
 
         <div className="text-secondary py-8 flex flex-col gap-2">
-          <Badge className="bg-chart-2">-20% off</Badge>
-          <h2 className="text-4xl font-semibold">Borderlands 4</h2>
+          <Badge className="bg-chart-2">{gameDeal.deal.cut} off</Badge>
+          <h2 className="text-4xl font-semibold">{gameDeal.title}</h2>
           <div className="flex gap-2">
-            <span className="line-through text-secondary/70">&#36;79.99</span>
-            <span className="font-medium">&#36;99.99</span>
+            <span className="line-through text-secondary/70">
+              &#36;{gameDeal.deal.regular.amount}
+            </span>
+            <span className="font-medium">
+              &#36;{gameDeal.deal.regular.amount}
+            </span>
           </div>
           <Button variant={"secondary"} className="w-32">
             View
