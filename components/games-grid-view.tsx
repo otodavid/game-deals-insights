23b8de96b2d4a392054b4c22855a1297dd32b975
otodavid <dieavid01@gmail.com>
2025-09-16 08:06:36 -0500

    fix: fixed undefined error

--- components/games-grid-view.tsx
+++ components/games-grid-view.tsx
@@ -4,16 +4,17 @@ import type { ITADDealsResponse } from "@/types/api-responses";
 export default function GamesGridView({ games }: { games: ITADDealsResponse }) {
   return (
     <div className="grid grid-cols-[repeat(auto-fill,minmax(200px,1fr))] gap-4">
-      {games?.list.map((game) => (
-        <GameCard
-          key={game.id}
-          id={game.id}
-          title={game.title}
-          assets={game.assets}
-          deal={game.deal}
-          type={game.type}
-        />
-      ))}
+      {games &&
+        games.list.map((game) => (
+          <GameCard
+            key={game.id}
+            id={game.id}
+            title={game.title}
+            assets={game.assets}
+            deal={game.deal}
+            type={game.type}
+          />
+        ))}
     </div>
   );
 }
