21e7158118921d2be4977b0c546053867dc8b547
otodavid <dieavid01@gmail.com>
2025-09-15 22:08:25 -0500

    feat:added grid and table view

---
+++ components/games-grid-view.tsx
@@ -0,0 +1,19 @@
+import GameCard from "./game-card";
+import type { ITADDealsResponse } from "@/types/api-responses";
+
+export default function GamesGridView({ games }: { games: ITADDealsResponse }) {
+  return (
+    <div className="grid grid-cols-[repeat(auto-fill,minmax(250px,1fr))] gap-4">
+      {games?.list.map((game) => (
+        <GameCard
+          key={game.id}
+          id={game.id}
+          title={game.title}
+          assets={game.assets}
+          deal={game.deal}
+          type={game.type}
+        />
+      ))}
+    </div>
+  );
+}
