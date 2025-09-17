a3140240da03b94bb4286eb9a02cdcb24a80d6c9
otodavid <dieavid01@gmail.com>
2025-09-17 08:35:36 -0500

    feat: add static images for shops and created mapping constant for each

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,5 +1,28 @@
 export const DEFAULT_DEALS_PARAMS = {
   filter: "N4IgLgngDgpiBcBtAjAXQDTmjAcgezAUQCZ0BmVAXyA%3D",
-  shops: '61,16,35,37,62',
-  country: 'CA',
+  shops: "61,16,35,37,62",
+  country: "CA",
+};
+
+export const DEFAULT_SHOPS: Record<number, { name: string; icon: string }> = {
+  61: {
+    name: "Steam",
+    icon: "/images/steam.png",
+  },
+  16: {
+    name: "Epic Games",
+    icon: "/images/epic.png",
+  },
+  35: {
+    name: "GOG",
+    icon: "/images/gog.png",
+  },
+  37: {
+    name: "Humble",
+    icon: "/images/humble.png",
+  },
+  62: {
+    name: "Ubisoft",
+    icon: "/images/gmg.png",
+  },
 };
