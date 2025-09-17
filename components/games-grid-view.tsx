f4d81ff790fdfacd5b765a1250bbdb3a56fb3a1a
otodavid <dieavid01@gmail.com>
2025-09-17 06:26:05 -0500

    feat: updated grid view to include deal modal for games

--- components/games-grid-view.tsx
+++ components/games-grid-view.tsx
@@ -1,20 +1,11 @@
-import GameCard from "./game-card";
 import type { ITADDealsResponse } from "@/types/api-responses";
+import GameCardModal from "./game-card-modal";
 
 export default function GamesGridView({ games }: { games: ITADDealsResponse }) {
   return (
     <div className="grid grid-cols-[repeat(auto-fill,minmax(200px,1fr))] gap-4">
       {games &&
-        games.list.map((game) => (
-          <GameCard
-            key={game.id}
-            id={game.id}
-            title={game.title}
-            assets={game.assets}
-            deal={game.deal}
-            type={game.type}
-          />
-        ))}
+        games.list.map((game) => <GameCardModal key={game.id} game={game} />)}
     </div>
   );
 }
