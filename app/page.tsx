ffdb6f0a3871acb74c0d4027ea443eac70be8a2c
otodavid <dieavid01@gmail.com>
2025-09-17 12:56:15 -0500

    feat: added responsivess

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,20 @@
 import GameDeals from "@/components/game-deals";
 import HeroSection from "@/components/hero";
+import InsightsSection from "@/components/insights-section";
 import PriceAlert from "@/components/price-alerts";
-import StatsCardList from "@/components/stats-card-list";
 
 export default function Home() {
   return (
     <div className="px-3 space-y-8 mt-8">
-      <div className="grid grid-cols-1 gap-8 2xl:grid-cols-[1fr_20rem] md:gap-8 justify-between">
-        <HeroSection />
-        <PriceAlert />
+      <div className="@container">
+        <div className="grid grid-cols-1 gap-8 @5xl:grid-cols-[1fr_.3fr] md:gap-8 justify-between items-stretch">
+          <HeroSection />
+          <PriceAlert />
+        </div>
       </div>
 
-      <div className="mb-8">
-        <StatsCardList />
+      <div className="mb-8 space-y-8">
+        <InsightsSection />
 
         <GameDeals />
       </div>
