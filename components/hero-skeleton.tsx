3a24aceb368b860c3b3a07c27d39d2f8beb98240
otodavid <dieavid01@gmail.com>
2025-09-17 20:33:32 -0500

    fix: fixed height style issue

--- components/hero-skeleton.tsx
+++ components/hero-skeleton.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { Skeleton } from "./ui/skeleton";
-import { Card, CardContent, CardHeader } from "./ui/card";
+import { Card } from "./ui/card";
 
 export default function HeroSkeleton() {
   return (
     <Card className="overflow-hidden p-4">
-      <Skeleton className="h-full w-full" />
+      <Skeleton className="h-full w-full min-h-80" />
     </Card>
   );
 }
